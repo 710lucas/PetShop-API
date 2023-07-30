04ffdbe6fcc168cdd53b58af83f84527b78d9de4
luxs710 <luxs710@gmail.com>
2023-07-30 12:13:17 -0300

    adicionando DTOs

---
+++ src/main/java/com/lux/Petshop/DTOs/AnimalDTO.java
@@ -0,0 +1,19 @@
+package com.lux.Petshop.DTOs;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimalDTO {
+
+    private String tamanho;
+    private String tamanhoPelo;
+    private String nome;
+
+}
---
+++ src/main/java/com/lux/Petshop/DTOs/ServicoDTO.java
@@ -0,0 +1,23 @@
+package com.lux.Petshop.DTOs;
+
+import com.lux.Petshop.Models.Animal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServicoDTO {
+
+    private Animal animal;
+    private Date data;
+    private String nome;
+    protected double precoP, precoM, precoG;
+    protected double precoPeloP, precoPeloM, precoPeloG;
+}
